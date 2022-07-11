902699eade96fb4342c3da74b9573923d14c50a7
devinreeks <devin@gotillit.com>
2022-07-11 19:02:34 +1000

    tailwind build is better

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**",
+    "created-form-lib/src/components/**",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
